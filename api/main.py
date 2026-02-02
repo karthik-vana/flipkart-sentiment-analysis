080e5e648d24a3ab7738e89700d3768cab98e850
karthik-vana <karthikvana236@gmail.com>
2026-02-02 09:54:22 +0530

    Optimize deployment size: remove pandas and uvicorn

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
-import pandas as pd
+
 import os
 import sys
 
